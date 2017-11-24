12645edae618fa0634cc679ffcc924b781d707f1
Pawel Kowaliszyn <pkowaliszyn@onet.eu>
2017-11-24 11:58:35 +0100

    Add files

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// scripts.js
+
+var a = prompt('Your value a is:'),
+	h = prompt('Your value h is:'),
+	triangleArea;
+
+triangleArea = a*h/2;
+
+alert('Triangle field with base a: ' + a + ' and height h: ' + h + ' is iqual to: ' + triangleArea);
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is iqual to: ' + triangleArea);
